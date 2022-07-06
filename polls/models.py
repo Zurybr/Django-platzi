442d4190a78128ddddc0c0a7bde7ac052763a677
Zurybr <brandom.ledesma@gmail.com>
2022-07-06 00:51:15 -0500

    migrations

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    #id
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+class Choices(models.Model):
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)#se elimina una question se van a eliminar todos los choices
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #User apps
+    'polls.apps.PollsConfig',
+    #Django native apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
