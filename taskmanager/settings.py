8caf34643b3669cfae88c03f7e78ae2845471532
Zurybr <brandom.ledesma@hotmail.com>
2022-07-16 17:44:39 -0500

    subida de seguridad

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -76,12 +76,31 @@ WSGI_APPLICATION = 'taskmanager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'new': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "nueva_test", #database
+        "USER": "postgres",
+        "PASSWORD":"postgres", 
+        "HOST": "127.0.0.1",
+        "PORT":"5432",
     }
 }
+
+#1 migrar los schemas| python manage.py migrate --database=new
+#2 Export data from the old Database| python manage.py dumpdata > data.json
+#3 Load Data into the new Database| python manage.py loaddata data.json --database=new
+#4 remove de old database in DATABASES| 
 
 
 # Password validation
